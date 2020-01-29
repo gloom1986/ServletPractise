23fe696dd6db85cf8917d014e84a04c920d3579e
ssergeev <gloom1986@mail.ru>
2020-01-29 16:03:59 +0300

    29.01.2020

--- ServletPractise/src/main/Java/com.ssergeev/ShopServlet.java
+++ ServletPractise/src/main/Java/com.ssergeev/ShopServlet.java
@@ -4,15 +4,35 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 
 public class ShopServlet extends HttpServlet {
+
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         PrintWriter printWriter = resp.getWriter();
+        String userName = req.getParameter("userName");
+        printWriter.print("<H1>Hello, " + userName +"</H1>");
+        printWriter.print("<div>Make your order:</div>");
         printWriter.print(
-                "<H1>" + "Hello, " + req.getParameter("userName") + "</H1>"
+            "<form action=\"/check\" method=\"POST\">" +
+                "<select name=\"item\" multiple>" +
+                    "<option value=\"1.0\">Matches (1,0$)</option>" +
+                    "<option value=\"5.5\">Book (5,5$)</option>" +
+                    "<option value=\"36.5\">Chair (36,5$)</option>" +
+                    "<option value=\"99.9\">Mobile phone (99,90$)</option>" +
+                    "<option value=\"49990\">Bentley (49990$)</option>" +
+                "</select>" + "<br>" +
+                "<button>Submit</button>" +
+            "</form>"
         );
+        printWriter.close();
     }
 }
--- ServletPractise/src/main/Java/com.ssergeev/WelcomeServlet.java
+++ ServletPractise/src/main/Java/com.ssergeev/WelcomeServlet.java
@@ -12,13 +12,15 @@ public class WelcomeServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html;charset=utf-8");
-        PrintWriter writer = resp.getWriter();
-        writer.print("<H1>" + "Welcome to Online shop" + "</H1>\n");
-        writer.print(
-                "<form action=\"/shop-servlet\" method=\"POST\">" +
-                        "<input name=\"userName\">" +
-                        "<button>" + "Enter" + "</button>" +
-                "</form>"
+        PrintWriter printWriter = resp.getWriter();
+        printWriter.print("<H1>Welcome to Online shop</H1>");
+        printWriter.print(
+            "<form action=\"/shop\" method=\"POST\">" +
+                "<input name=\"userName\">" + "<br>" +
+                "<button>" + "Enter" + "</button>" +
+            "</form>"
         );
+        printWriter.close();
+
     }
 }
